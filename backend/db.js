e85e3087a6b5dd97db837cd987f29e4b2fe37c8a
jeansonvillanueva <jeansonhelario24@gmail.com>
2026-04-11 14:10:57 +0800

    debbug the db.js const db

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2';
 
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: 'mysql.ymca.ph',
   user: 'ymcaph_user',
   password: 'e8f133def539f610fe95fa789ac08d6ee8f133def539f610fe95fa789ac08d6e',
   database: 'ymcaph_db'
